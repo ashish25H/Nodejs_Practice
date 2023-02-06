95783b4e693b90be3e508246f5356bbc8f940b01
ashish25H <me.ashishkp@gmail.com>
2023-02-06 20:10:36 +0530

    node

---
+++ Module/ES6modules/main.mjs
@@ -0,0 +1,9 @@
+// import add from './math.mjs';
+import {add,subtract} from "./math.mjs";
+
+// console.log(math.add(2,3));
+// console.log(math.subtract(5,1));
+
+// const {add,subtract} = math;
+console.log(add(1,1));
+console.log(subtract(1,1));
---
+++ Module/index.js
@@ -0,0 +1,7 @@
+const add = require('./add');
+const subtract = require('./subtract');
+
+console.log(add(1,22));
+console.log('hello');
+
+console.log(subtract(5,1));
